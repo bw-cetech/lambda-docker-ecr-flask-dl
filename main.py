4056dea21a317fc87aa98927d2707390cf1b433c
bw-cetech <105169661+bw-cetech@users.noreply.github.com>
2023-04-18 00:59:51 +0100

    updated app variable to main

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ import os
 
 from werkzeug.utils import secure_filename
 
-app = flask.Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 # increased as bad request with large images
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif']
-app.config['UPLOAD_PATH'] = 'static/uploads'
+main = flask.Flask(__name__)
+main.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 # increased as bad request with large images
+main.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif']
+main.config['UPLOAD_PATH'] = 'static/uploads'
 
 def validate_image(stream):
     header = stream.read(1024)  # increased from 512 as bad request with large images
@@ -26,30 +26,30 @@ def validate_image(stream):
         return None
     return '.' + format # (format if format != 'jpeg' else 'jpg') ASSUMES JPEG AND JPG FORMAT CAN BOTH BE UPLOADED
 
-@app.route('/')
+@main.route('/')
 def index():
-    files = os.listdir(app.config['UPLOAD_PATH'])
+    files = os.listdir(main.config['UPLOAD_PATH'])
     return flask.render_template('index.html', files=files)
 
-@app.route('/', methods=['POST'])
+@main.route('/', methods=['POST'])
 def upload_files():
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+        if file_ext not in main.config['UPLOAD_EXTENSIONS'] or \
                 file_ext != validate_image(uploaded_file.stream):
             abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        uploaded_file.save(os.path.join(main.config['UPLOAD_PATH'], filename))
         #model = Model()
         # for testing
         # return filename
         return flask.render_template("index.html", token=model.runInference(filename))
     return redirect(url_for('index'))
 
-@app.route('/uploads/<filename>')
+@main.route('/uploads/<filename>')
 def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+    return send_from_directory(main.config['UPLOAD_PATH'], filename)
 
 def handler(event, context):
     return serverless_wsgi.handle_request(main, event, context)
