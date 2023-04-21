7efc7ac96ea1bc0092d281c368f6d28fc0b3f8a0
bw-cetech <105169661+bw-cetech@users.noreply.github.com>
2023-04-21 18:48:58 +0100

    Image.open(urllib.request

--- main.py
+++ main.py
@@ -94,18 +94,16 @@ def upload_files():
         import io
         import imageio.v3 as iio # NB v3 needed to avoid this error: imageio.core.legacy_plugin_wrapper.LegacyPlugin.read() got multiple values for keyword argument 'index'
         # image_url = 'https://github.com/bw-cetech/lambda-docker-ecr-flask-dl/blob/bf3e205ff91ef7202cb067552d3685f33cf6e9b4/static/uploads/00015_00010_00027.png?raw=true'
-        
-        from requests_file import FileAdapter
-
-        s = requests.Session()
-        s.mount(app.config['UPLOAD_FOLDER'], FileAdapter())
-
-        response = s.get(os.path.join(app.config['UPLOAD_FOLDER'], filename)
         # response = requests.get(image_url ) # for testing
-        response.raise_for_status()
-        f = io.BytesIO(response.content)
-        img = iio.imread(f, index=None) # instead of below
+        # response.raise_for_status()
+        # f = io.BytesIO(response.content)
+        # img = iio.imread(f, index=None) # instead of below
         # img = load_img(f, color_mode='rgb', target_size=(224, 224)) # DOESNT WORK but filename instead of f works
+
+        import urllib
+        from PIL import Image
+
+        img = Image.open(urllib.request.urlopen(os.path.join(app.config['UPLOAD_FOLDER'], filename)))     
         img = Image.fromarray(img).resize((224, 224))
 
         # os.chdir(currentPath) # change back
